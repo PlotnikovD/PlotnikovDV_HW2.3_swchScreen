84870d94cb301ae2cb491a3cd628655f43876fe8
D.Plotnikov <plotnikdima5296@gmail.com>
2021-10-27 19:30:16 +0500

    wihout main alert

--- PlotnikovDV_HW2.3_swchScreen/ViewController.swift
+++ PlotnikovDV_HW2.3_swchScreen/ViewController.swift
@@ -9,10 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    var alertMessageName = "Your name is User 😉"
+    var alertMessagePassword = "Your password is Password 😎"
+ 
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+            performSegue(withIdentifier: "deteilSegue", sender: nil)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? WelcomeViewController else { return }
+        dvc.login = loginTF.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func showAlertButtonTapped(message: String) {
+
+            let alert = UIAlertController(title: "Oops!", message: message, preferredStyle: UIAlertController.Style.alert)
+
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+            self.present(alert, animated: true, completion: nil)
+        }
+    
+    @IBAction func forgotNameButton() {
+        showAlertButtonTapped(message: alertMessageName)
+    }
+    
+    @IBAction func forgotPasswordButton() {
+        showAlertButtonTapped(message: alertMessagePassword)
+    }
+    
 
 
 }
---
+++ PlotnikovDV_HW2.3_swchScreen/WelcomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeViewController.swift
+//  PlotnikovDV_HW2.3_swchScreen
+//
+//  Created by Dmitrii on 27.10.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    var login: String?
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = self.login else { return }
+        label.text = "Hello \(login)"
+    }
+    
+
+    @IBAction func goBackTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}
